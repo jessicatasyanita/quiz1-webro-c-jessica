4254a1522994ae9e35cc24240af21e505ad34193
Jessica Tasyanita <jessica.tasyanita@gmail.com>
2021-09-23 21:17:54 +0700

    local food page unfinished

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,24 +55,7 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-// const sections = document.querySelectorAll('section[id]');
 
-// function scrollActive(){
-//     const scrollY = window.pageYOffset;
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight;
-//         const sectionTop = current.offsetTop - 50;
-//         sectionId = current.getAttribute('id');
-
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 function scrollHeader(){
@@ -120,3 +103,22 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
+
+/* */
+
+let swiper = new Swiper('.food__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+});
